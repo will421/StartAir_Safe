49b9b8de28a8cb2d944d3e98f29232449f8c9236
corentinricou <corentin.ricou@gmail.com>
2014-01-27 10:36:30 +0100

    Empty server, with empty dialog box

---
+++ server/Resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// fichier Include Microsoft Visual C++.
+// Utilisé par server.rc
+//
+#define IDR_MAINFRAME					128
+#define IDD_SERVER_DIALOG				102
+#define IDR_HTML_SERVER_DIALOG			104
+#define IDP_SOCKETS_INIT_FAILED			103
+
+// Valeurs par défaut suivantes des nouveaux objets
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		101
+#define _APS_NEXT_COMMAND_VALUE		32771
+#endif
+#endif
